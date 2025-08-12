226d2f7f9cdc35e1a2b198c5cf55ddfdb66da426
Isaac Almeida <isaacalmeida.dev@gmail.com>
2025-08-11 22:43:30 -0300

    Correção no firebaseConfig

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,14 +1,19 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'SUA_API_KEY',
-  authDomain: 'SEU_AUTH_DOMAIN',
-  projectId: 'SEU_PROJECT_ID',
-  storageBucket: 'SEU_STORAGE_BUCKET',
-  messagingSenderId: 'SEU_MESSAGING_SENDER_ID',
-  appId: 'SEU_APP_ID',
+  apiKey: "AIzaSyCPKuw5mr033AT6YBuGVzeKIRtY5xLrjJ4",
+  authDomain: "alimentamais-4bdf4.firebaseapp.com",
+  projectId: "alimentamais-4bdf4",
+  storageBucket: "alimentamais-4bdf4.firebasestorage.app",
+  messagingSenderId: "484799326297",
+  appId: "1:484799326297:web:00b667313823b06e512948"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = getAuth(app);   // <-- export nomeado
+export default app;
